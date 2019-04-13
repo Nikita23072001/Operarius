86c1b3355243618cdc4d21daaefac5e37deb3faf
Mesiya391 <33184329+Mesiya391@users.noreply.github.com>
2019-04-13 14:36:28 +0200

    navbar and background added

--- Operarius/src/app/app.module.ts
+++ Operarius/src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { HeaderComponent } from './background/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+  NavbarComponent,
+HeaderComponent]
 })
 export class AppModule { }
